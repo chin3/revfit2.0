159c849e8d9592e0f9aa51ada5162aeb71e9ba13
Patrick Boronski <plboronski@gmail.com>
2020-09-02 16:59:23 -0400

    Added timer page + bonus back end stuff

--- RevFitNG/src/app/components/workoutcomponents/workouttimer/workouttimer.component.ts
+++ RevFitNG/src/app/components/workoutcomponents/workouttimer/workouttimer.component.ts
@@ -12,14 +12,17 @@ const CURR_WORKOUT = "CURR";
 })
 export class WorkouttimerComponent implements OnInit {
 
-  constructor(private ws : WorkoutService, private wes : WorkoutExerciseService) { }
+  constructor(private ws: WorkoutService, private wes: WorkoutExerciseService) { }
   currWorkout: Workout;
   interval;
-  timeLeft: number = 5;
-  setsAndReps : WorkoutExercise[];
+  curExerciseIndex: number = 0;
+  curExerciseInitTime : number;
+  timeLeft: number;
+  done : boolean = false;
+  setsAndReps: WorkoutExercise[];
 
   ngOnInit(): void {
-    this.currWorkout= JSON.parse(sessionStorage.getItem(CURR_WORKOUT));
+    this.currWorkout = JSON.parse(sessionStorage.getItem(CURR_WORKOUT));
     console.log(this.currWorkout);
     this.ws.getWorkoutTime(this.currWorkout.id).subscribe(
       (response) => {
@@ -32,23 +35,45 @@ export class WorkouttimerComponent implements OnInit {
         console.log(this.setsAndReps);
       }
     );
+    
   }
 
   startTimer() {
+    this.curExerciseInitTime = this.setsAndReps[this.curExerciseIndex].time;
+    console.log(this.curExerciseInitTime);
     this.interval = setInterval(() => {
-      if(this.timeLeft > 0) {
+      if (this.timeLeft >= 0) {
+        this.exerciseTimer();
         this.timeLeft--;
+
       } else {
-        //this.timeLeft = 60;
         clearInterval(this.interval);
+        this.done = true;
       }
-    },1000)
+    }, 1000)
   }
   pauseTimer() {
     clearInterval(this.interval);
   }
-  toHome(){
-    window.location.href="/home";
+  toHome() {
+    window.location.href = "/home";
+  }
+
+  exerciseTimer() {
+    if (this.setsAndReps[this.curExerciseIndex].time > 1) {
+      this.setsAndReps[this.curExerciseIndex].time--;
+    }
+    else if (this.setsAndReps[this.curExerciseIndex].sets > 1) {
+      this.setsAndReps[this.curExerciseIndex].sets--;
+      this.setsAndReps[this.curExerciseIndex].time = this.curExerciseInitTime;
+    }
+    else if (this.curExerciseIndex < this.currWorkout.exercises.length) {
+      this.setsAndReps[this.curExerciseIndex].time--;
+      this.setsAndReps[this.curExerciseIndex].sets--;
+      this.curExerciseIndex++;
+      this.curExerciseInitTime = this.setsAndReps[this.curExerciseIndex].time;
+    }
+
   }
 
 }
