02e42ed7dca0a5b3279159703858bd0f75f247e7
Patrick Boronski <plboronski@gmail.com>
2020-09-06 18:49:37 -0400

    Adding chrome driver to project

--- revfit/src/test/java/com/revature/selenium/HomePageTests.java
+++ revfit/src/test/java/com/revature/selenium/HomePageTests.java
@@ -28,7 +28,7 @@ public class HomePageTests {
 	@BeforeSuite
 	public void setUp() {
 
-		File file = new File("D:\\Program Files\\Selenium\\chromedriver.exe");
+		File file = new File("src/test/resources/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
 		driver = new ChromeDriver();
